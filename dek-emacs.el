289a52776eb484614db51431a371d648863361ff
Davis Kirkendall <davis.kirkendall@rwth-aachen.de>
2014-07-22 01:22:33 +0200

    add align repeat

--- dek-emacs.el
+++ dek-emacs.el
@@ -373,6 +373,12 @@ expand-region cruft."
   (replace-regexp "(:,:,\\([12]\\))" "\\1")
   (replace-regexp "\\(.+?,.+?\\),.*" "\\1"))
 
+(defun align-repeat (start end regexp)
+  "Repeat alignment with respect to
+     the given regular expression."
+  (interactive "r\nsAlign regexp: ")
+  (align-regexp start end
+                (concat "\\(\\s-*\\)" regexp) 1 1 t))
 
 
 
